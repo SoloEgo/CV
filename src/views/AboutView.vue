55da9cb0bc904ebffef784e09ee738b6f384da8d
Egor <solovev_ea@metholding.com>
2022-10-17 08:00:44 +0300

    add pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is page about me</h1>
+    <h2><i class="bi bi-cone-striped"></i> page is under construction</h2>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
+.about{
+  margin-top: 1rem;
 }
 </style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="portfolio">
-      <h1>This is a portfolio page</h1>
-    </div>
-  </template>
+  <div class="portfolio">
+    <h1>This is a portfolio page</h1>
+    <h2><i class="bi bi-cone-striped"></i> page is under construction</h2>
+  </div>
+</template>
   
-  <style>
-
-  </style>
+<style>
+.portfolio {
+  margin-top: 1rem;
+}
+</style>
   
